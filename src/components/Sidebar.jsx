77b83935b251288649789d4bc9d57fda6d106edb
Anton Plauche <antonplauche92@gmail.com>
2023-04-23 17:10:57 -0500

    single page rough design and sidebar tweaks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,21 +6,26 @@ export default function Sidebar(){
 
 
   return (
-    <aside className="bg-neutral-dark text-white overflow-y-auto">
-      <div className="my-12 mx-8">
-        LOGO
-      </div>
-      <nav className="w-full flex flex-col">
-        <NavLink className="navlink" to="/">
-          Home
-        </NavLink>
-        <NavLink className="navlink" to="/browse">
-          Browse
-        </NavLink>
-        <NavLink className="navlink" to="/collection">
-          My Collection
-        </NavLink>
-      </nav>
-    </aside>
+    <div className="bg-neutral-dark text-white overflow-y-auto flex flex-col justify-between">
+      <header>
+        <div className="my-12 mx-8">
+          LOGO
+        </div>
+        <nav className="w-full flex flex-col">
+          <NavLink className="navlink" to="/">
+            Home
+          </NavLink>
+          <NavLink className="navlink" to="/browse">
+            Browse
+          </NavLink>
+          <NavLink className="navlink" to="/collection">
+            My Collection
+          </NavLink>
+        </nav>
+      </header>
+      <footer className="py-8 px-8 border-t border-neutral-700 text-neutral-400">
+        <p>Data from the Art Institute of Chicago public API.</p>
+      </footer>
+    </div>
   )
 }
--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,58 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 
 export default function SinglePage(){
 
+  const { id:artID } = useParams()
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [imageBaseUrl, setImageBaseUrl] = useState('')
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+
+        const { data: res } = await axios.get(`https://api.artic.edu/api/v1/artworks/${artID}?fields=id,title,image_id,date_end,place_of_origin,artist_display,exhibition_history`);
+
+        setImageBaseUrl(res.config.iiif_url) 
+
+        setData(res.data);
+
+        setLoading(false)
+
+      } catch (error) {
+        // setError(error); 
+        console.log(error)
+      } 
+    };
+
+    fetchData();
+
+  },[])
+
+
+  if(loading){
+    return (
+      <div>Loading...</div>
+    )
+  }
+
   return (
-    <div>this is a single art piece</div>
+    <div className="max-w-[720px] mx-auto bg-white p-8 rounded-lg shadow-md">
+      <h1 className="font-bold text-4xl mb-8">{data.title}</h1>
+      <img crossOrigin="true" className="" src={`${imageBaseUrl}/${data.image_id}/full/843,/0/default.jpg`} alt={data.title} width={"100%"} />
+      <div className="py-8">
+        <p className="mb-8">{data.artist_display}</p>
+        {data.exhibition_history && (
+          <p>
+            <strong>Exhibition History: </strong>
+            {data.exhibition_history}
+          </p>
+        )}
+      </div>
+    </div>
   )
 }
