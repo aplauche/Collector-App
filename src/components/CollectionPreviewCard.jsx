64575a4baccdae845dba68133a895a35fd4e7a1e
Anton Plauche <antonplauche92@gmail.com>
2023-04-25 21:23:32 -0500

    add link to collection tiles

--- src/components/CollectionPreviewCard.jsx
+++ src/components/CollectionPreviewCard.jsx
@@ -1,20 +1,26 @@
+import { HiOutlineEye } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function CollectionPreviewCard({item, imageBaseUrl}){
+
+
 
   const addImageFallback = (event) => {
     event.currentTarget.src = '/fallback.jpg';
   };
 
-
   return (
       <div>
-        <div className="cover-image cover-image--square">
+        <Link to={`artwork/${item.id}`} className="collection-preview cover-image cover-image--square block">
           {item.image_id ? (
             <img crossOrigin="true" className="rounded-md" src={`${imageBaseUrl}/${item.image_id}/full/843,/0/default.jpg`} onError={addImageFallback} alt="" width={"100%"} />
           ) : (
             <img className="rounded-md" src="/fallback.jpg" alt="" width={"100%"} />
           )}
-        </div>
+          <div className="collection-preview-cover opacity-0  rounded-md transition-opacity w-full h-full absolute top-0 left-0 bg-black/50 flex justify-center items-center text-white font-bold">
+            <HiOutlineEye className="text-4xl text-white " />
+          </div>
+        </Link>
       </div>
   )
 }
