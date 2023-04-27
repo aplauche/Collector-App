f3f7bd590b90f9462913b93a4876af915002c53d
Anton Plauche <antonplauche92@gmail.com>
2023-04-26 20:06:33 -0500

    add page transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import Single from "./pages/Single"
 import BrowsePage from "./pages/Browse"
 import CollectionPage from "./pages/Collection"
 import NotFound from "./pages/NotFound"
+import { motion, AnimatePresence } from "framer-motion"
 
+
+function RouteTransitionWrapper({element}){
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        {element}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
 
 function App() {
 
@@ -14,12 +32,24 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/browse" element={<BrowsePage />} />
-            <Route path="/browse/:id" element={<BrowsePage />} />
-            <Route path="/artwork/:id" element={<Single />} />
-            <Route path="/collection" element={<CollectionPage />} />
-            <Route path="*" element={<NotFound />} />
+            <Route index element={
+              <RouteTransitionWrapper element={<Home />} />
+            } />
+            <Route path="/browse" element={
+              <RouteTransitionWrapper element={<BrowsePage />} />
+            } />
+            <Route path="/browse/:id" element={
+              <RouteTransitionWrapper element={<BrowsePage />} />
+            } />
+            <Route path="/artwork/:id" element={
+              <RouteTransitionWrapper element={<Single />} />
+            } />
+            <Route path="/collection" element={
+              <RouteTransitionWrapper element={<CollectionPage />} />
+            } />
+            <Route path="*" element={
+              <RouteTransitionWrapper element={<NotFound />} />
+            } />
           </Route>
         </Routes>
 
